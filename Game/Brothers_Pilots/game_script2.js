ce10bce1b0ddfe2eb40dd166e58bb633c3fddc8e
Matvey <glazyrin_2017@inbox.ru>
2022-01-24 22:10:11 +1000

    Инициализировать проекта

---
+++ Game/Brothers_Pilots/game_script2.js
@@ -0,0 +1,119 @@
+document.getElementById("start").addEventListener("click", onStartGame);
+document.getElementById("chit").addEventListener("click", onEndGame);
+
+let max = 5;
+let array = [];
+let points = 0;
+let moves = 0;
+const cells = document.querySelectorAll('.cell');
+let hack = true;
+
+newGame();
+cells.forEach(iterator => {
+    iterator.addEventListener('mousedown', () => {
+        for (i=1;i<max;i++){
+            if (array[i][iterator.id.charAt(1)] != 0) {
+                array[i][iterator.id.charAt(1)] = 0;
+                points--;
+            }
+            else {
+                array[i][iterator.id.charAt(1)] = 1;
+                points++;
+            }
+        }
+        for (i=1;i<max;i++){
+            if (array[iterator.id.charAt(0)][i] != 0) {
+                array[iterator.id.charAt(0)][i] = 0;
+                points--;
+            }
+            else {
+                array[iterator.id.charAt(0)][i] = 1;
+                points++;
+            }
+        }
+        if(array[iterator.id.charAt(0)][iterator.id.charAt(1)] != 0) {
+            array[iterator.id.charAt(0)][iterator.id.charAt(1)] = 0;
+            points--;
+        }
+        else{
+            array[iterator.id.charAt(0)][iterator.id.charAt(1)] = 1;
+            points++;
+        }
+        changeCells();
+    })
+})
+
+function newGame(){
+    points = 0;
+    moves = 0;
+    array = [];
+    for(i=1; i<max; i++)
+    {
+        array[i] = [];
+    }
+    for(i=1; i<max; i++){
+        for(j=1; j<max; j++){
+            array[i][j] = 0;
+            document.getElementById(i.toString() + j).style.background = ('#f0f8ff');
+        }
+    }
+    document.getElementById('point').innerText = "Число ходов: " + moves;
+    if (!hack){
+        document.getElementById('chit').disabled = true;
+        document.getElementById('chit').style.backgroundColor = '#bdb76b';
+    }else{
+        document.getElementById('chit').disabled = false;
+        document.getElementById('chit').style.backgroundColor = '#f0f8ff';
+    }
+
+}
+
+function changeCells(){
+    moves++;
+    cells.forEach(iterator => {
+        for (i=1; i<max; i++){
+            for (j=1; j<max; j++){
+                if(array[i][j] == 1){
+                    document.getElementById(i.toString() + j).style.background = ('#9B4222');
+                }else{
+                    document.getElementById(i.toString() + j).style.background = ('#f0f8ff');
+                }
+            }
+        }
+    })
+    document.getElementById('point').innerText = "Число ходов: " + moves;
+    setTimeout(checkVictory, 1);
+}
+
+function onStartGame(){
+    newGame();
+}
+
+//кнопка для быстрой победы
+function onEndGame(){
+    cells.forEach(iterator => {
+        for (i=1; i<max; i++){
+            for (j=1; j<max; j++){
+                document.getElementById(i.toString() + j).style.background = ('#9B4222');
+                array[i][j] = 1;
+            }
+        }
+    })
+    points = 16
+    setTimeout(checkVictory, 1);
+}
+
+function checkVictory(){
+    if (points == 16){
+        if (moves == 1){
+            alert("Вы смогли победить за " + moves + " ход, поздравляю;)");
+        } else if (moves > 1 && moves < 5){
+            alert("Вы смогли победить за " + moves + " хода, поздравляю;)");
+        }else {
+            alert("Вы смогли победить за " + moves + " ходов, поздравляю;)");
+        }
+        if (hack)
+            hack = true;
+        newGame();
+    }
+}
---
+++ Game/Whack_a_Mole/game_script1.js
@@ -0,0 +1,61 @@
+document.getElementById("start").addEventListener("click", onStartGame);
+
+let points = 0;
+let startTime = 1000;
+let changeTime = 50;
+let time;
+let gameEvent = [];
+let maxMole = 15
+let idMole;
+
+function onStartGame(){
+    disabledButton(true)
+    points = 0;
+    idMole = null;
+    document.getElementById('point').innerText = "Очки: 0";
+    time = startTime;
+    gameEvent = setInterval(() => {
+        if (points < 12){
+            spawn();
+        }else {
+            alert('Вы победили;) Хотите сыграть ещё?');
+            clearInterval(gameEvent);
+            disabledButton(false);
+            document.getElementById('point').innerText = "Необходимо набрать 12 очков";
+        }
+    }, time + 10)
+}
+
+function disabledButton(count){
+    if (count){
+        document.getElementById('start').disabled = true;
+        document.getElementById('start').style.backgroundColor = '#bdb76b';
+    }else{
+        document.getElementById('start').style.backgroundColor = '#ffebcd';
+        document.getElementById('start').disabled = false;
+    }
+}
+
+function  spawn(){
+    idMole = Math.floor(Math.random()*(maxMole + 1));
+    document.getElementById(idMole.toString()).onclick = whack;
+    document.getElementById(idMole.toString()).style.background = '#9B4222';
+    //accessOfMole(idMole, true);
+    sleep(time).then(() => {
+        document.getElementById(idMole.toString()).style.background = '#f0f8ff';
+        document.getElementById(idMole.toString()).onclick = null;
+        //accessOfMole(idMole, false);
+    })
+}
+
+function sleep(count){
+    return new Promise(resolve => setTimeout(resolve, count));
+}
+
+function whack() {
+    document.getElementById(idMole.toString()).style.background = '#f0f8ff';
+    document.getElementById(idMole.toString()).onclick = null;
+    points++;
+    time -= changeTime;
+    document.getElementById('point').innerText = "Очки: " + points;
+}
